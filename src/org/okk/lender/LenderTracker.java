017e01547797c3f0011286152e80efc9fbff67b9
Gunnar Söderman <c558744@bofthew.com>
2010-09-17 00:15:41 +0200

    more code

---
+++ src/org/okk/lender/LenderTracker.java
@@ -0,0 +1,13 @@
+package org.okk.lender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LenderTracker extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
